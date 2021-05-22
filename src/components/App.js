9cdc0997afa5ede40206cacb20d1264978421688
donellgill <donell.gill@outlook.com>
2021-05-22 15:57:11 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, {useState, useEffect} from "react"
+
+
+
+function App() {
+    const [randomDog, setRandomDog] = useState(null);
+
+    useEffect(() => {
+            fetch("https://dog.ceo/api/breeds/image/random") 
+          .then((r) => r.json())
+          .then((data) => {
+            setRandomDog(data.messages);
+          });
+    },
+         []);
+
+        if(!randomDog) return <p>Loading . . . </p>;
+
+            return <img src = {randomDog} alt = "A random dog"></img>
+    
+}
+
+export default App;
